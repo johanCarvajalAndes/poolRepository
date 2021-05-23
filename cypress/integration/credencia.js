1e10cb506cab971c4f6d5480923e96dcdac189fe
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2021-05-23 16:33:22 -0500

    comentarios en credencial

--- cypress/integration/credencia.js
+++ cypress/integration/credencia.js
@@ -3,5 +3,6 @@ export const Email = "j.carvajalm@uniandes.edu.co"
 export const Password = "jQ6KHZdu5wf!Fd!"//jQ6KHZdu5wf!Fd!//SDXzE_iif*2sT7D#
 export const Url = "http://localhost:2369"
 export const User= "Johan C"
-export const numberScenarios = 8;
-export const numberScenariosPages = 16;
+export const numberScenarios = 8; // 10 escenarios utilizan este 8x10 = 80 reducir para ejecutarlo menos veces
+export const numberScenariosPages = 16; //2 escenarios utilizan esta variable para un for 16x2 = 32  reducir para ejecutarlo menos veces, 
+// los escenarios a priori utilizan el tamaño del array de los archivos estaticos. 
