816a6fb02fc218a03ea7035d0350eba92367d081
AndresSalas <a.salasr@uniandes.edu.co>
2021-05-22 22:29:32 -0500

    se adiciona iteraciones para Tags

--- cypress/integration/credencia.js
+++ cypress/integration/credencia.js
@@ -1,5 +1,5 @@
-export const Email = "j.carvajalm@uniandes.edu.co"
-export const Password = "SDXzE_iif*2sT7D"//jQ6KHZdu5wf!Fd!//SDXzE_iif*2sT7D#
-export const Url = "http://localhost:2370"
-export const User= "Johan Fernando Carvajal Marín"
+export const Email = "leo1_salas@hotmail.com"
+export const Password = "Admin12345"//jQ6KHZdu5wf!Fd!//SDXzE_iif*2sT7D#
+export const Url = "http://localhost:2368"
+export const User= "Andres"
 export const numberScenarios = 8;
